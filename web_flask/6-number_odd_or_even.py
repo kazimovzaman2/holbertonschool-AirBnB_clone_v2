40c4c3dbce43c79003aa2d7cb2e1b76f151e2951
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-11 12:03:35 +0400

    change name of template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,7 +50,7 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """Print n odd or even"""
-    return render_template("6-number.html", n=n)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
