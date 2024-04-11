2e492edc9c41178b5d414998eff9f7e37064d6a6
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-11 12:58:58 +0400

    Changes for task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,29 +2,26 @@
 """
 Script that starts a Flask web application:
 """
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_data(self):
-    """
-    Closes the database connection after each request.
-    """
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """
-    Renders a template that displays a list of states.
+    States are sorted by name.
     """
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 if __name__ == "__main__":
