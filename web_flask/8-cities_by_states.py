8069d2930837f42612751425ce65b4a63d9ea886
Zaman Kazimov <kazimovzaman2@gmail.com>
2024-04-14 05:59:29 +0400

    task 8 done

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,17 +5,8 @@ Script that starts a Flask web application:
 from flask import Flask
 from flask import render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_data(self):
-    """
-    Closes the database connection after each request.
-    """
-    storage.close()
 
 
 @app.route("/cities_by_states", strict_slashes=False)
@@ -23,8 +14,16 @@ def cities_by_states():
     """
     Renders a template that displays a list of cities by states.
     """
-    states = storage.all(State)
+    states = sorted(storage.all("State").values(), key=lambda s: s.name)
+    for state in states:
+        state.cities = sorted(state.cities, key=lambda c: c.name)
     return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
 
 
 if __name__ == "__main__":
